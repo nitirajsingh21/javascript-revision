05b8d84ada692b86c4ef2f4793d50a3a5fe2f306
nitirajsingh21 <140580411+nitirajsingh21@users.noreply.github.com>
2025-10-06 19:21:18 +0000

    conversion of datatypes

---
+++ 01basic/operation.js
@@ -0,0 +1,12 @@
+let score="33abc";
+console.log(typeof score);
+let value=Number(score);
+console.log(typeof value);
+console.log(value);
+//nan result//
+// "33"=> 33
+// "33abc"=>NaN
+// true=>1
+// 1=> true
+//""=>false
+//"hitesh"=>true
