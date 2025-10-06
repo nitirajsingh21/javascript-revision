9aff3b777087755b7361817dda54f6995c4f60dc
nitirajsingh21 <140580411+nitirajsingh21@users.noreply.github.com>
2025-10-06 19:00:21 +0000

    commit

---
+++ 01basic/datatype.js
@@ -0,0 +1,12 @@
+"use strict";
+//alert() we r using node js not browser//
+//code readability should be maintained//
+// number=>2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+// object
+console.log(typeof null)
---
+++ 01basic/variable.js
@@ -0,0 +1,8 @@
+const id="1234"; //we cant change id
+let email="singhsaab";
+city="jaipur"
+let accountstate;
+/*
+dont use var becausse of use in blocke scope and functional scope
+*/
+console.table([id,email,accountstate]);
